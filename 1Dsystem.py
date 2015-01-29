780e62709d8201192737eaf1cd06224d5c656665
Tom Strange <tpstrange@gmail.com>
2015-01-29 16:38:11 +0000

    End of day commit. Look at replacing Euler method for N1 with RK4

--- 1Dsystem.py
+++ 1Dsystem.py
@@ -21,13 +21,13 @@ v = spc.c/n #transport velocity
 D = v*l/3. #diffusion coeffecient
 
 
-J = 100 #number of space steps
+J = 10 #number of space steps
 L = 8.e-4 #length of slap
 dx = L/J #space steps across slap
-z = np.arange(0,L-dx,dx) # array of space steps
+z = np.arange(0,L,dx) # array of space steps
 
-N = 100 #number of time steps
-T = 1e-6 #length of time
+N = 1000 #number of time steps
+T = 1e-3 #length of time
 dt = T/N #time steps
 
 
@@ -83,10 +83,12 @@ for timestep in range(N):
 	I_G_vals = I_G(timestep*dt)
 	I_R_vals = I_R(timestep*dt)
 
-
 	W_G_new = np.dot(A_G, (B_G.dot(W_G) + f(N_1,W_G,I_G_vals)) )
+	#W_G_new = np.linalg.solve(A_G, (B_G.dot(W_G) + f(N_1,W_G,I_G_vals)) )
 	W_R_new = np.dot(A_R, (B_R.dot(W_R) + g(N_1,W_R,I_R_vals)) )
+	#W_R_new = np.linalg.solve(A_R, (B_R.dot(W_R) + g(N_1,W_R,I_R_vals)) )
 	W_A_new = np.dot(A_A,  (B_A.dot(W_A) + h(N_1,W_A)) )
+	#W_A_new = np.linalg.solve(A_A,  (B_A.dot(W_A) + h(N_1,W_A)) )
 	N_1_new = N_1 + dt*q(N_1, W_G, W_R, W_A)
 
 	W_G = W_G_new
@@ -99,10 +101,16 @@ for timestep in range(N):
 		W_R_store = W_R
 		W_A_store = W_A
 		N_1_store = N_1
+		I_G_store = I_G_vals
+		I_R_store = I_R_vals
+		temp_store = f(N_1,W_G,I_G_vals)
 	else:
 		W_G_store = np.vstack((W_G_store, W_G))
 		W_R_store = np.vstack((W_R_store, W_R))
 		W_A_store = np.vstack((W_A_store, W_A))
 		N_1_store = np.vstack((N_1_store, N_1))
+		I_G_store = np.vstack((I_G_store, I_G_vals))
+		I_R_store = np.vstack((I_R_store, I_R_vals))
+		temp_store = np.vstack((temp_store,f(N_1,W_G,I_G_vals)))
 
 	print(timestep,end='\r')
