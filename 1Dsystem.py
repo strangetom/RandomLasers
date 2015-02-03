c5325fa4f53a713e70b92491d2d6a299162f61e7
Tom Strange <tpstrange@gmail.com>
2015-02-03 09:55:22 +0000

    Use a more realistic value of kappa_e. Adjust average intensity to assume 1 pulse only. (Previous calculation used peak power, not average)

--- 1Dsystem.py
+++ 1Dsystem.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy import constants as spc
-from numba import jit
+#from numba import jit, vectorize
 
 #define constants
 sig_abs = 3.0e-24 #absorption cross section
@@ -8,46 +8,52 @@ sig_em = 3.0e-23 #emission cross section
 tau_e = 3.2e-6 #lifetime of excited state
 N_t = 1.6e25 #total concentration of laser particles
 l = 40.e-6 #transport mean free path
-kappa_e = 0.1e-9 #extinction coefficient
+kappa_e = 3.e3 #extinction coefficient (see spec sheet in shared folder)
 tau_G = 14.e-9 #pump pulse FWHM
 tau_R = 14.e-9 #proble pulse FWHM 
 t_G = 0. #time of maxima of pump pulse 
 t_R = 0. #time of maxima of probe pulse 
 illum_area = np.pi*(2.e-3)**2 #illumation area
-I_G0 = 200.e-3/illum_area/tau_G #average pump intensity
-I_R0 = 200.e-3/illum_area/tau_R #average probe intensity
+I_G0 = 200.e-3/illum_area/1 #average pump intensity. !!USING E/tau_G is the peak power, not average. We need the frequency of the pulses.!!
+I_R0 = 200.e-3/illum_area/1 #average probe intensity
 n = 1.35 #average refractive index of medium
 c = spc.c/n #speed of light in medium
 v = spc.c/n #transport velocity
 D = v*l/3. #diffusion coeffecient
 
 
-J = 10 #number of space steps
+J = 4 #number of space steps
 L = 8.e-4 #length of slap
 dx = L/J #space steps across slap
 z = np.arange(0,L,dx) # array of space steps
 
-N = 1000 #number of time steps
-T = 1e-3 #length of time
+N = 100 #number of time steps
+T = 1e-7 #length of time
 dt = T/N #time steps
 
 #Define beta value
 beta = D*dt/(2.*dx**2)
 
+
 def f(N_1, W_G, I_G_vals):
 	return -1*sig_abs*v*(N_t-N_1)*W_G + I_G_vals/l
+
 
 def g(N_1, W_R, I_R_vals):
 	return sig_em*v*N_1*W_R + I_R_vals/l
 
+
 def h(N_1, W_A):
 	return sig_em*v*N_1*W_A + N_1/tau_e
+
 
 def q(N_1, W_G, W_R, W_A):
 	return sig_abs*v*(N_t-N_1)*W_G - sig_em*v*N_1*(W_R+W_A) - N_1/tau_e
 
+
 def I_G(t):
 	return I_R0*np.sqrt(4*np.log(2)/np.pi)*np.exp(-kappa_e*z)*np.exp(-4*np.log(2)*(t-t_G-z/c)**2/(tau_G**2))
+
 
 def I_R(t):
 	return I_R0*np.sqrt(4*np.log(2)/np.pi)*np.exp(-kappa_e*z)*np.exp(-4*np.log(2)*(t-t_R-z/c)**2/(tau_R**2))
@@ -83,11 +89,8 @@ for timestep in range(N):
 	I_R_vals = I_R(timestep*dt)
 
 	W_G_new = np.dot(A_G, (B_G.dot(W_G) + f(N_1,W_G,I_G_vals)) )
-	#W_G_new = np.linalg.solve(A_G, (B_G.dot(W_G) + f(N_1,W_G,I_G_vals)) )
 	W_R_new = np.dot(A_R, (B_R.dot(W_R) + g(N_1,W_R,I_R_vals)) )
-	#W_R_new = np.linalg.solve(A_R, (B_R.dot(W_R) + g(N_1,W_R,I_R_vals)) )
 	W_A_new = np.dot(A_A, (B_A.dot(W_A) + h(N_1,W_A)) )
-	#W_A_new = np.linalg.solve(A_A,  (B_A.dot(W_A) + h(N_1,W_A)) )
 	N_1_new = N_1 + dt*q(N_1, W_G, W_R, W_A)
 
 	W_G = W_G_new
@@ -102,7 +105,6 @@ for timestep in range(N):
 		N_1_store = N_1
 		I_G_store = I_G_vals
 		I_R_store = I_R_vals
-		temp_store = f(N_1,W_G,I_G_vals)
 	else:
 		W_G_store = np.vstack((W_G_store, W_G))
 		W_R_store = np.vstack((W_R_store, W_R))
@@ -110,10 +112,6 @@ for timestep in range(N):
 		N_1_store = np.vstack((N_1_store, N_1))
 		I_G_store = np.vstack((I_G_store, I_G_vals))
 		I_R_store = np.vstack((I_R_store, I_R_vals))
-		temp_store = np.vstack((temp_store,f(N_1,W_G,I_G_vals)))
-
-	if timestep == 1:
-		break
 
 
 	print(timestep,end='\r')
