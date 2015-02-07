68e78f593ae0c6b7738413bdc67f3aec16ee7ba4
Tom Strange <tpstrange@gmail.com>
2015-02-07 16:38:12 +0000

    Define figure size. Rename variable read from file to be more generic. Remove extra bracket that broke the plotting.

--- 3Dplot.py
+++ 3Dplot.py
@@ -2,11 +2,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-fig = plt.figure()
+fig = plt.figure(figsize=(20,10))
 ax = fig.add_subplot(111, projection='3d')
 plt.hold(True)	
 
-W_G_store = np.loadtxt("./Data/W_G_store.txt", delimiter=',')
+store = np.loadtxt("./Data/W_G_store.txt", delimiter=',')
 
 l = 40.e-6
 L = 0.8e-3 #length of slap
@@ -16,7 +16,7 @@ x = np.arange(0,L+2*l+dx,dx) # array of space steps
 N = 1000000 #number of time steps
 T = 5.e-8 #length of time
 dt = T/N #time steps
-time = ( dt*500*np.arange(0, W_G_store.shape[0], 1) ) *1e9 #time array in nanoseconds
+time = ( dt*500*np.arange(0, store.shape[0], 1) ) *1e9 #time array in nanoseconds
 
 """
 #If you fancy it multicoloured
@@ -32,8 +32,8 @@ for i in range(len(tableau20)):
 """
 
 for i in range(x.shape[0]):
-	position = ( i*dx*np.ones(W_G_store.shape[0]) ) *1e3 # position in microns
-	ax.plot(xs=time, ys=W_G_store[:,i] ,zs=position, zdir='y', color='r'])
+	position = ( i*dx*np.ones(store.shape[0]) ) *1e3 # position in microns
+	ax.plot(xs=time, ys=store[:,i] ,zs=position, zdir='y', color='r')
 
 ax.set_xlabel('Time (ns)')
 ax.set_xlim(0., 30)
@@ -42,6 +42,6 @@ ax.set_ylim(0, 0.88)
 ax.set_zlabel('Energy density')
 ax.set_zlim(0, 0.004)
 ax.set_title('Pump pulse energy density')
+fig.savefig('Graph.png')
 
-
-plt.show()
+#plt.show()
