ca469a06718225cfb9a161333b7e8cb00c6d7197
Tom Strange <tpstrange@gmail.com>
2015-02-21 18:09:32 +0000

    Move megaplot.py and update megaplot to label each plot

--- .graphs/21Feb/megaplot.py
+++ .graphs/21Feb/megaplot.py
@@ -5,6 +5,7 @@ from scipy import constants as spc
 c = spc.c/1.35 # speed of light in medium
 E_A = 6.63e-34*c/700e-9
 
+#Load data
 flux1x4 = 4.e10/(c*E_A*1e22)*np.loadtxt('./Data/L=1/Flux.I=4e10.L=1.txt',delimiter=',')
 flux1x8 = 8.e10/(c*E_A*1e22)*np.loadtxt('./Data/L=1/Flux.I=8e10.L=1.txt',delimiter=',')
 flux1x16 = 16.e10/(c*E_A*1e22)*np.loadtxt('./Data/L=1/Flux.I=16e10.L=1.txt',delimiter=',')
@@ -31,9 +32,11 @@ N3x16 = np.loadtxt('./Data/L=3/N_pop.I=16e10.L=3.txt',delimiter=',')
 
 time = (5.e-13 * 50 * np.arange(0, flux1x4.shape[0], 1)) * 1e9
 
-fig, ax = plt.subplots(3,3,figsize=(10,10))
+#Setup axes
+fig, ax = plt.subplots(3,3,figsize=(12,12))
 fig.subplots_adjust(hspace = .001, wspace = .001)
 
+#Plot everything
 ax[0,0].plot(time, flux1x16,'r')
 ax[0,0].set_ylim((0,20))
 ax[0,0].set_xticklabels(())
@@ -121,8 +124,22 @@ ax2.plot(time, N3x4[:,12],'b')
 ax2.set_yticks(np.arange(0, 0.3, 0.1))
 ax2.set_ylim(0,0.3)
 
+#Axis labels
 fig.text(0.5, 0.05, 'Time (ns)', ha='center', va='center',fontsize=18)
-fig.text(0.08, 0.5, 'Flux ($10^{22}s^{-1})$', ha='center', va='center',rotation='vertical',fontsize=18)
+fig.text(0.08, 0.5, 'Flux ($10^{22}s^{-1}$)', ha='center', va='center',rotation='vertical',fontsize=18)
 fig.text(0.95, 0.5, 'Excitation level', ha='center', va='center',rotation=270,fontsize=18)
+
+#Label each figure
+fig.text(0.26, 0.325, r'$L = 1 mm$''\n'r'$I_{G0} = 4\times10^{10}Wm^{-2}$')
+fig.text(0.26, 0.59, r'$L = 1 mm$''\n'r'$I_{G0} = 8\times10^{10}Wm^{-2}$')
+fig.text(0.26, 0.855, r'$L = 1 mm$''\n'r'$I_{G0} = 16\times10^{10}Wm^{-2}$')
+
+fig.text(0.52, 0.325, r'$L = 2 mm$''\n'r'$I_{G0} = 4\times10^{10}Wm^{-2}$')
+fig.text(0.52, 0.59, r'$L = 2 mm$''\n'r'$I_{G0} = 8\times10^{10}Wm^{-2}$')
+fig.text(0.51, 0.855, r'$L = 2 mm$''\n'r'$I_{G0} = 16\times10^{10}Wm^{-2}$')
+
+fig.text(0.78, 0.325, r'$L = 3 mm$''\n'r'$I_{G0} = 4\times10^{10}Wm^{-2}$')
+fig.text(0.78, 0.59, r'$L = 3 mm$''\n'r'$I_{G0} = 8\times10^{10}Wm^{-2}$')
+fig.text(0.77, 0.855, r'$L = 3 mm$''\n'r'$I_{G0} = 16\times10^{10}Wm^{-2}$')
 
 fig.savefig('1Dsystem length and pump variation.png')
