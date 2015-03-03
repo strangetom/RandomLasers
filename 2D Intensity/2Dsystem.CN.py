26b368a5e4b3e643bf41466bd4a3f2b5cbcf77ee
Tom Strange <tpstrange@gmail.com>
2015-03-03 19:05:52 +0000

    Gut the probe from the code. It isn't used.

--- 2D Intensity/2Dsystem.CN.py
+++ 2D Intensity/2Dsystem.CN.py
@@ -39,13 +39,11 @@ dt = T/N # time increment
 
 # initial conditions
 W_G = np.zeros((J,J))
-W_R = np.zeros((J,J))
 W_A = np.zeros((J,J))
 N_pop = np.zeros((J,J))
 
 # lists for storage
 W_G_storage = []
-W_R_storage = []
 W_A_storage = []
 N_pop_storage = []
 I_G_storage = []
@@ -71,33 +69,16 @@ def ASE_LHS(N_mid_step):
 	M = np.diagflat([0]+[ -dt*D/(2*dz**2) for i in range(J-2)], -1) + np.diagflat([1]+[ (1. + 2*dt*D/(2*dz**2) - dt/4*sig_em*v*N_mid_step[i][i]*N_t) for i in range(J-2)]+[1]) + np.diagflat([ -dt*D/(2*dz**2) for i in range(J-2)]+[0], 1)
 	return M
 
-def PROBE_RHS(W_R, N_pop, I_R):
-	"""Calculates the right hand side of the PDE for the pump"""
-	M = np.diagflat([0]+[ dt*D/(2*dz**2) for i in range(J-2)], -1) + np.diagflat([0]+[(1. - 2*dt*D/(2*dz**2) + dt/4*sig_em*v*N_t*N_mid_step[i][i]) for i in range(J-2) ]+[0]) + np.diagflat([ dt*D/(2*dz**2) for i in range(J-2)]+[0], 1)
-	return M.dot(W_R) + dt/tau_e *I_R
-
-def PROBE_LHS(N_mid_step):
-	"""Calculates the left hand side of the PDE for the pump"""
-	M = np.diagflat([0]+[ -dt*D/(2*dz**2) for i in range(J-2)], -1) + np.diagflat([1]+[ (1. + 2*dt*D/(2*dz**2) - dt/4*sig_em*v*N_t*N_mid_step[i][i]) for i in range(J-2)]+[1]) + np.diagflat([ -dt*D/(2*dz**2) for i in range(J-2)]+[0], 1)
-	return M
-
-
-def POP_RHS(N_pop, W_G, W_A, W_R):
+def POP_RHS(N_pop, W_G, W_A):
 	"""Calculates the right hand side of the PDE for the excited population"""
 	term_1 = dt/2*sig_abs*v*(1-N_pop)*I_G0/(c*E_G) * W_G
-	term_2 = dt/2*sig_em*v*N_pop*I_G0/c * (W_A/E_A + W_R/E_R)
+	term_2 = dt/2*sig_em*v*N_pop*I_G0/c * W_A/E_A
 	term_3 = dt/2/tau_e * N_pop
 	return N_pop + (term_1 - term_2 - term_3)
 
 def I_G(t):
 	"""Calculates the intensity through space at a given time"""
 	I_vec = c*tau_e/l * np.sqrt(4*np.log(2)/np.pi)*np.exp(-kappa_e*z)*np.exp( -4*np.log(2)*(t-t_G-z/c)**2/tau_G**2)
-	I_vec[0]=I_vec[-1] = 0
-	return np.zeros((J,J)) + I_vec.reshape(J,1)
-
-def I_R(t):
-	"""Calculates the intensity through space at a given time"""
-	I_vec = I_R0/I_G0*c*tau_e/l * np.sqrt(4*np.log(2)/np.pi)*np.exp(-kappa_e*z)*np.exp( -4*np.log(2)*(t-t_R-z/c)**2/tau_R**2)
 	I_vec[0]=I_vec[-1] = 0
 	return np.zeros((J,J)) + I_vec.reshape(J,1)
 
@@ -110,19 +91,16 @@ for timestep in range(N):
 
 	# get intensity spatial profile at n+0.25
 	I_G_mid_step = I_G(timestep*dt + dt/4)
-	I_R_mid_step = I_R(timestep*dt + dt/4)
 
 	# calculate intermediate time steps for variables
-	N_pop_next_half = POP_RHS(N_pop, W_G, W_A, W_R)
+	N_pop_next_half = POP_RHS(N_pop, W_G, W_A)
 	N_mid_step = (N_pop_next_half + N_pop)/2
 
 	# Calculate premultiplying matrices for the pump and ASE
 	A_pump = PUMP_LHS(N_mid_step)
-	A_probe = PROBE_LHS(N_mid_step)
 	A_ASE = ASE_LHS(N_mid_step)
 
 	W_G_mid_step = np.linalg.solve(A_pump, PUMP_RHS(W_G.T, N_mid_step.T, I_G_mid_step.T) )
-	W_R_mid_step = np.linalg.solve(A_probe, PROBE_RHS(W_R.T, N_mid_step.T, I_R_mid_step.T) )
 	W_A_mid_step = np.linalg.solve(A_ASE, ASE_RHS(W_A.T, N_mid_step.T) )
 
 	"""
@@ -132,19 +110,16 @@ for timestep in range(N):
 
 	# get intensity spatial profile at n+0.75
 	I_G_mid_step = I_G(timestep*dt + 3*dt/4)
-	I_R_mid_step = I_R(timestep*dt + 3*dt/4)
 
 	# calculate next time steps for variables
-	N_pop_next = POP_RHS(N_pop_next_half, W_G_mid_step, W_A_mid_step, W_R_mid_step)
+	N_pop_next = POP_RHS(N_pop_next_half, W_G_mid_step, W_A_mid_step)
 	N_mid_step = (N_pop_next + N_pop_next_half)/2
 
 	# Calculate premultiplying matrices for the pump and ASE
 	A_pump = PUMP_LHS(N_mid_step.T)
-	A_probe = PROBE_LHS(N_mid_step.T)
 	A_ASE = ASE_LHS(N_mid_step.T)
 
 	W_G_next = (np.linalg.solve(A_pump, PUMP_RHS(W_G_mid_step, N_mid_step, I_G_mid_step) ))
-	W_R_next = (np.linalg.solve(A_probe, PROBE_RHS(W_R_mid_step, N_mid_step, I_R_mid_step) ))
 	W_A_next = (np.linalg.solve(A_ASE, ASE_RHS(W_A_mid_step, N_mid_step) ))
 
 	"""
@@ -153,7 +128,6 @@ for timestep in range(N):
 	# set newly calculated values to current time for next loop
 	N_pop = N_pop_next
 	W_G = W_G_next
-	W_R = W_R_next
 	W_A = W_A_next
 
 	if timestep % 100 == 0:
@@ -161,7 +135,6 @@ for timestep in range(N):
 		I_G_storage.append(I_G_mid_step)
 		N_pop_storage.append(N_pop)
 		W_G_storage.append(W_G)
-		W_R_storage.append(W_R)
 		W_A_storage.append(W_A)
 
 	print(timestep, end='\r')
@@ -172,7 +145,6 @@ I_G_storage = np.array(I_G_storage)
 N_pop_storage = np.array(N_pop_storage)
 W_G_storage = np.array(W_G_storage)
 W_A_storage = np.array(W_A_storage)
-W_R_storage = np.array(W_R_storage)
 Flux = (W_A_storage[:,0])
 """
 np.savetxt('./Data/L=2/W_A.I=4e10.L=2.txt',W_A_storage[:,int(J/2)], delimiter=',',newline='\n')
