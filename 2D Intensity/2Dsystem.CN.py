5fe4556edc9c860bd9ebc14d14d30e3cfe778a51
Tom Strange <tpstrange@gmail.com>
2015-03-05 15:11:18 +0000

    Add modifications to allow non-square shapes. This is done by modifying the matrices and including N_t as a matrix that set values at given x coordinates to 0 always. This keeps the simple square geometry but only allows excitiation along a single strip down the centre.

--- 2D Intensity/2Dsystem.CN.py
+++ 2D Intensity/2Dsystem.CN.py
@@ -5,7 +5,6 @@ from scipy import constants as spc
 sig_abs = 3.0e-24 # absorption cross section
 sig_em = 3.0e-23 # emission cross section
 tau_e = 3.2e-6 # lifetime of excited state
-N_t = 1.6e25 # total concentration of laser particles
 l = 100.e-6 # transport mean free path
 kappa_e = 5.e-4 # extinction coefficient (see spec sheet in shared folder)
 tau_G = 14.e-9 # pump pulse FWHM
@@ -20,35 +19,32 @@ D = v*l/3. # diffusion coeffecient
 
 E_G = 6.63e-34*c/532e-9 # energy of pump photons
 E_A = 6.63e-34*c/700e-9 # energy of emitted photons
-I_G0 = 4.e10 # average pump intensity
+I_G0 = 8.e10 # average pump intensity
 
 #define space parameters
-X = 0.002 # length of medium in x direction
-dx = l/2 # space increment
-x = np.arange(-l/2, X+l/2+dx, dx) # vector in space
-x[0] = x[-1] = 0 # modifiy space vector so pulses don't exist outside medium
-J = x.shape[0] # number of space steps
-Y = 0.003 # length of medium in y direction
-dy = l/2 # space increment
-y = np.arange(-l/2, Y+l/2+dx, dy) # vector in space
-y[0] = y[1] # modifiy space vector so pulses don't decay before entering medium
-y[-1] = y[-2] # modifiy space vector so pulses don't decay after exiting medium
-I = y.shape[0] # number of space steps
+L = 0.003 # length of medium
+dz = l/2 # space increment
+z = np.arange(-l/2, L+l/2+dz, dz) # vector in space
+z[0] = z[1] # modifiy space vector so pulses don't decay before entering medium
+z[-1] = z[-2] # modifiy space vector so pulses don't decay after exiting medium
+J = z.shape[0] # number of space steps
 
-# define time parameters
+
+# q is the number of space steps in from the sides that we're removing
+q = 15
+# change the shape of N_t
+N_t = np.vstack( (np.zeros((q,J)), 1.6e25*np.ones((J-2*q,J)), np.zeros((q,J)) ) ) # total concentration of laser particles
+
+
+#define time parameters
 N = 100000 # number of time steps
 T = 50.e-9 # length of time
 dt = T/N # time increment
 
 # initial conditions
-W_G = np.zeros((J,I))
-W_A = np.zeros((J,I))
-N_pop = np.zeros((J,I))
-
-# boundary conditions fix
-#F = np.vstack( (np.zeros((1,I)), np.ones((J-2,I)), np.zeros((1,I)) ) )
-F = np.ones((J,I))
-F[:,0]=F[:,-1] = 0
+W_G = np.zeros((J,J))
+W_A = np.zeros((J,J))
+N_pop = np.zeros((J,J))
 
 # lists for storage
 W_G_storage = []
@@ -57,36 +53,41 @@ N_pop_storage = []
 I_G_storage = []
 
 # function definitions
-def PUMP_RHS(W_G, N_pop, I_G, x_deriv_flag):
+def PUMP_RHS(W_G, N_pop, I_G, y_deriv_constant):
 	"""Calculates the right hand side of the PDE for the pump"""
-	if x_deriv_flag:
-		M = np.diagflat([0]+[ dt*D/(2*dx**2) for j in range(J-2)], -1) + np.diagflat([0]+[(1. - 2*dt*D/(2*dx**2)) for j in range(J-2) ]+[0]) + np.diagflat([ dt*D/(2*dx**2) for j in range(J-2)]+[0], 1)
+	if y_deriv_constant:
+		M = np.diagflat([0]+[ dt*D/(2*dz**2) for i in range(J-2)], -1) + np.diagflat([0]+[(1. - 2*dt*D/(2*dz**2)) for i in range(J-2) ]+[0]) + np.diagflat([ dt*D/(2*dz**2) for i in range(J-2)]+[0], 1)
+		return M.dot(W_G.T).T - dt/4*sig_abs*v*N_t*(1-N_pop)*W_G + dt/tau_e *I_G
+	elif ~y_deriv_constant:
+		M = np.diagflat([0]*(q+1)+[ dt*D/(2*dz**2) for i in range(J-1-2*q)]+(q-1)*[0], -1) + np.diagflat([0]*q+[(1. - 2*dt*D/(2*dz**2)) for i in range(J-2*q) ]+q*[0]) + np.diagflat([0]*(q-1)+[ dt*D/(2*dz**2) for i in range(J-1-2*q)]+(q+1)*[0], 1)
 		return M.dot(W_G) - dt/4*sig_abs*v*N_t*(1-N_pop)*W_G + dt/tau_e *I_G
-	elif ~x_deriv_flag:
-		#M = np.diagflat([0]+[ dt*D/(2*dx**2) for i in range(I-2)], -1) + np.diagflat([0]+[(1. - 2*dt*D/(2*dx**2)) for i in range(I-2) ]+[0]) + np.diagflat([ dt*D/(2*dx**2) for i in range(I-2)]+[0], 1)
-		#print((M.dot(W_G.T) - dt/4*sig_abs*v*N_t*(1-N_pop.T)*W_G.T + dt/tau_e *I_G.T ).T)
-		#return ( M.dot(W_G.T) - dt/4*sig_abs*v*N_t*(1-N_pop.T)*W_G.T + dt/tau_e *I_G.T )
-		# This takes the transpose of all the matrices to do the necessary calculations
-		# We transpose it back to the original shape in order to solve the matrix equation. This means the PUMP_LHS equation needn't change greatly.
 
-def PUMP_LHS(N_pop, x_deriv_flag):
+def PUMP_LHS(N_mid_step, y_deriv_constant):
 	"""Calculates the left hand side of the PDE for the pump"""
-	M = np.diagflat([0]+[ -dt*D/(2*dx**2) for j in range(J-2)], -1) + np.diagflat([1]+[ (1. + 2*dt*D/(2*dx**2)) for j in range(J-2)]+[1]) + np.diagflat([ -dt*D/(2*dx**2) for j in range(J-2)]+[0], 1)
-	return M + dt/4*sig_abs*v*N_t*(1-N_pop)
+	if y_deriv_constant:
+		M = np.diagflat([0]+[ -dt*D/(2*dz**2) for i in range(J-2)], -1) + np.diagflat([1]+[ (1. + 2*dt*D/(2*dz**2) + dt/4*sig_abs*v*N_t[i][i]*(1-N_mid_step[i][i])) for i in range(J-2)]+[1]) + np.diagflat([ -dt*D/(2*dz**2) for i in range(J-2)]+[0], 1)
+		return M
+	elif ~y_deriv_constant:
+		M = np.diagflat([0]*(q+1)+[ -dt*D/(2*dz**2) for i in range(J-1-2*q)]+(q-1)*[0], -1) + np.diagflat([1]*q+[ (1. + 2*dt*D/(2*dz**2) + dt/4*sig_abs*v*N_t[i][i]*(1-N_mid_step[i][i])) for i in range(J-2*q)]+q*[1]) + np.diagflat([0]*(q-1)+[ -dt*D/(2*dz**2) for i in range(J-1-2*q)]+(q+1)*[0], 1)
+		return M 
 
-def ASE_RHS(W_A, N_pop, x_deriv_flag):
+def ASE_RHS(W_A, N_mid_step, y_deriv_constant):
 	"""Calculates the right hand side of the PDE for amplified spontaneous emission"""
-	if x_deriv_flag:
-		M = np.diagflat([0]+[ dt*D/(2*dx**2) for j in range(J-2)], -1) + np.diagflat([0]+[ (1. - 2*dt*D/(2*dx**2)) for j in range(J-2)]+[0]) + np.diagflat([ dt*D/(2*dx**2) for j in range(J-2)]+[0], 1)
-		return M.dot(W_A) + dt/4*sig_em*v*N_pop*N_t*W_A + dt*c*N_t*E_A/tau_e/I_G0 * N_pop
-	elif ~x_deriv_flag:
-		M = np.diagflat([0]+[ dt*D/(2*dx**2) for i in range(I-2)], -1) + np.diagflat([0]+[ (1. - 2*dt*D/(2*dx**2)) for i in range(I-2)]+[0]) + np.diagflat([ dt*D/(2*dx**2) for i in range(I-2)]+[0], 1)
-		return ( M.dot(W_A.T) + dt/4*sig_em*v*N_t*N_pop.T*W_A.T + dt*c*N_t*E_A/tau_e/I_G0 * N_pop.T )
+	if y_deriv_constant:
+		M = np.diagflat([0]+[ dt*D/(2*dz**2) for i in range(J-2)], -1) + np.diagflat([0]+[ (1. - 2*dt*D/(2*dz**2)) for i in range(J-2)]+[0]) + np.diagflat([ dt*D/(2*dz**2) for i in range(J-2)]+[0], 1)
+		return M.dot(W_A.T).T + dt/4*sig_em*v*N_mid_step*N_t*W_A + dt*c*N_t*E_A/tau_e/I_G0 * N_mid_step
+	elif ~y_deriv_constant:
+		M = np.diagflat([0]*(q+1)+[ dt*D/(2*dz**2) for i in range(J-1-2*q)]+(q-1)*[0], -1) + np.diagflat([0]*q+[ (1. - 2*dt*D/(2*dz**2)) for i in range(J-2*q)]+q*[0]) + np.diagflat([0]*(q-1)+[ dt*D/(2*dz**2) for i in range(J-1-2*q)]+(q+1)*[0], 1)
+		return M.dot(W_A) + dt/4*sig_em*v*N_mid_step*N_t*W_A + dt*c*N_t*E_A/tau_e/I_G0 * N_mid_step
 
-def ASE_LHS(N_pop):
+def ASE_LHS(N_mid_step, y_deriv_constant):
 	"""Calculates the left hand side of the PDE for amplified spontaneous emission"""
-	M = np.diagflat([0]+[ -dt*D/(2*dx**2) for i in range(J-2)], -1) + np.diagflat([1]+[ (1. + 2*dt*D/(2*dx**2)) for i in range(J-2)]+[1]) + np.diagflat([ -dt*D/(2*dx**2) for i in range(J-2)]+[0], 1)
-	return M - dt/4*sig_em*v*N_t*N_pop
+	if y_deriv_constant:
+		M = np.diagflat([0]+[ -dt*D/(2*dz**2) for i in range(J-2)], -1) + np.diagflat([1]+[ (1. + 2*dt*D/(2*dz**2) - dt/4*sig_em*v*N_mid_step[i][i]*N_t[i][i]) for i in range(J-2)]+[1]) + np.diagflat([ -dt*D/(2*dz**2) for i in range(J-2)]+[0], 1)
+		return M
+	elif ~y_deriv_constant:
+		M = np.diagflat([0]*(q+1)+[ -dt*D/(2*dz**2) for i in range(J-1-2*q)]+(q-1)*[0], -1) + np.diagflat([1]*q+[ (1. + 2*dt*D/(2*dz**2) - dt/4*sig_em*v*N_mid_step[i][i]*N_t[i][i]) for i in range(J-2*q)]+q*[1]) + np.diagflat([0]*(q-1)+[ -dt*D/(2*dz**2) for i in range(J-1-2*q)]+(q+1)*[0], 1)
+		return M
 
 def POP_RHS(N_pop, W_G, W_A):
 	"""Calculates the right hand side of the PDE for the excited population"""
@@ -97,11 +98,11 @@ def POP_RHS(N_pop, W_G, W_A):
 
 def I_G(t):
 	"""Calculates the intensity through space at a given time"""
-	I_vec = c*tau_e/l * np.sqrt(4*np.log(2)/np.pi)*np.exp(-kappa_e*y)*np.exp( -4*np.log(2)*(t-t_G-y/c)**2/tau_G**2)
+	I_vec = c*tau_e/l * np.sqrt(4*np.log(2)/np.pi)*np.exp(-kappa_e*z)*np.exp( -4*np.log(2)*(t-t_G-z/c)**2/tau_G**2)
 	I_vec[0]=I_vec[-1] = 0
-	return np.zeros((J,I)) + I_vec
+	return np.zeros((J,J)) + I_vec.reshape(J,1)
 
-for timestep in range(1):
+for timestep in range(N):
 
 	"""
 	First we calculate the n+0.5 step.
@@ -116,11 +117,11 @@ for timestep in range(1):
 	N_mid_step = (N_pop_next_half + N_pop)/2
 
 	# Calculate premultiplying matrices for the pump and ASE
-	A_pump = PUMP_LHS(N_mid_step)
-	A_ASE = ASE_LHS(N_mid_step)
+	A_pump = PUMP_LHS(N_mid_step, True)
+	A_ASE = ASE_LHS(N_mid_step, True)
 
-	W_G_mid_step = np.linalg.solve(A_pump, PUMP_RHS(W_G, N_mid_step, I_G_mid_step, True) )
-	W_A_mid_step = np.linalg.solve(A_ASE, ASE_RHS(W_A, N_mid_step, True) )
+	W_G_mid_step = np.linalg.solve(A_pump, PUMP_RHS(W_G, N_mid_step, I_G_mid_step, True) ) 
+	W_A_mid_step = np.linalg.solve(A_ASE, ASE_RHS(W_A, N_mid_step, True) ) 
 
 	"""
 	Then we complete the calculation to get the n+1 step.
@@ -135,11 +136,11 @@ for timestep in range(1):
 	N_mid_step = (N_pop_next + N_pop_next_half)/2
 
 	# Calculate premultiplying matrices for the pump and ASE
-	A_pump = PUMP_LHS(N_mid_step)
-	A_ASE = ASE_LHS(N_mid_step)
+	A_pump = PUMP_LHS(N_mid_step, False)
+	A_ASE = ASE_LHS(N_mid_step, False)
 
-	W_G_next = np.linalg.solve(A_pump, PUMP_RHS(W_G_mid_step, N_mid_step, I_G_mid_step, False) )
-	W_A_next = np.linalg.solve(A_ASE, ASE_RHS(W_A_mid_step, N_mid_step, False) )
+	W_G_next = (np.linalg.solve(A_pump, PUMP_RHS(W_G_mid_step, N_mid_step, I_G_mid_step, False) )).T
+	W_A_next = (np.linalg.solve(A_ASE, ASE_RHS(W_A_mid_step, N_mid_step, False) )).T
 
 	"""
 	Set newly calculated data ready for next loop and save data
@@ -170,8 +171,6 @@ np.savetxt('./Data/L=2/W_A.I=4e10.L=2.txt',W_A_storage[:,int(J/2)], delimiter=',
 np.savetxt('./Data/L=2/N_pop.I=4e10.L=2.txt',N_pop_storage[:,int(J/2)], delimiter=',',newline='\n')
 np.savetxt('./Data/L=2/Flux.I=4e10.L=2.txt',np.sum(Flux,axis=1), delimiter=',',newline='\n')
 """
-"""
 np.save('N_pop', N_pop_storage)
 np.save('W_A', W_A_storage)
 np.save('W_G', W_G_storage)
-"""
