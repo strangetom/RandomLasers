02d6f0d69e0d856989f57a0cffaff3495d886ddb
Tom Strange <tpstrange@gmail.com>
2015-03-19 16:08:12 +0000

    Correct function arguments

--- 1D Field/1Dsystem.opt.py
+++ 1D Field/1Dsystem.opt.py
@@ -9,8 +9,8 @@ w_a = 2*np.pi*6e14 # centre frequency
 tau_32 = 1e-13 # lifetime of level 3
 tau_21 = 1e-9 # lifetime of level 2
 tau_10 = 1e-11 # lifetime of level 1
-T_2 = 2.18e-14 # mean time between dephasing events
-P_r = 1e25 # pumping rate
+T_2 = 2.18e-14 # mean time between dephasing events, collision time
+P_r = 1e7 # pumping rate
 
 # Fundamental constants
 hbar = spc.hbar
@@ -72,7 +72,7 @@ N3_storage = []
 
 # Function definitions
 @nb.jit(nopython=True)
-def update_P(P, P_prev):
+def update_P(P, P_prev, E, N1, N2):
 	for position in range(1, E.shape[0], 1):
 		P[position] = dt**2*gamma_r*e**2/(gamma_c*m_e*(1+dw_a*dt))*(N1[position]-N2[position])*E[position] + (1+dt*dw_a-w_a**2*dt**2)/(1+dw_a*dt)*P[position] - P_prev[position]/(1+dw_a*dt)
 	return P
@@ -86,32 +86,32 @@ def update_H(E, H):
 @nb.jit(nopython=True)
 def update_E(E, H, P, P_prev, medium_mask):
 	for position in range(1, E.shape[0], 1):
-		if medium_mask[position] == 1:
+		if medium_mask[position] > 1:
 			E[position] = E[position] + dt/(epsilon_0*dx)*(H[position]-H[position-1]) + (P_prev[position] - P[position])/epsilon_0
 		else:
 			E[position] = E[position] + dt/(4*epsilon_0*dx)*(H[position]-H[position-1]) + (P_prev[position] - P[position])/(4*epsilon_0)
-		#E[int(E.shape[0]/2)] += 1e10*np.exp(-(timestep)**2 /(1e-16)**2.)
-		return E
+	E[int(E.shape[0]/2)] += 1e15*np.exp(-(timestep)**2 /(1e-10)**2.)
+	return E
 
 @nb.jit(nopython=True)
-def update_N(N0, N1, N2, N3, medium_mask):
-	for position in range(1, E.shape[0], 1):
-		#if medium_mask[position] == 1:
-		N3[position] = N3[position] + dt*(P_r*N0[position]-N3[position]/tau_32)
-		N2[position] = N2[position] + E[position]/(hbar*w_a)*( (P[position]-P_prev[position])/dt - N2[position]/tau_21)
-		N1[position] = N1[position] - E[position]/(hbar*w_a)*( (P[position]-P_prev[position])/dt - N1[position]/tau_10)
-		N0[position] = N0[position] + dt*(N1[position]/tau_10 - P_r*N0[position])
-		return N0, N1, N2, N3
+def update_N(N0, N1, N2, N3, medium_mask, E, P, P_prev):
+	for position in range(1, N0.shape[0], 1):
+		if medium_mask[position] > 0:
+			N3[position] = N3[position] + dt*(P_r*N0[position]-N3[position]/tau_32)
+			N2[position] = N2[position] + E[position]/(hbar*w_a)*( (P[position]-P_prev[position])/dt - N2[position]/tau_21)
+			N1[position] = N1[position] - E[position]/(hbar*w_a)*( (P[position]-P_prev[position])/dt - N1[position]/tau_10)
+			N0[position] = N0[position] + dt*(N1[position]/tau_10 - P_r*N0[position])
+	return N0, N1, N2, N3
 
-for timestep in range(250000):
+for timestep in range(300000):
 
 	P_copy = P.copy()
-	P = update_P(P, P_prev)
+	P = update_P(P, P_prev, E, N1, N2)
 	P_prev = P_copy
 	H = update_H(E, H)
 	E = update_E(E, H, P, P_prev, medium_mask)
 
-	N0, N1, N2, N3 = update_N(N0, N1, N2, N3, medium_mask)
+	N0, N1, N2, N3 = update_N(N0, N1, N2, N3, medium_mask, E, P, P_prev)
 
 	if timestep % 100 == 0:
 		#store data in storage list
