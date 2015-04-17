0d22f8fa4cf07f27b07e23b1980b8e17d29059e9
Tom Strange <tpstrange@gmail.com>
2015-04-17 22:00:57 +0100

    Remove svg and update 1D megaplot colours to tablaeu20

--- .graphs/21Feb/megaplot.py
+++ .graphs/21Feb/megaplot.py
@@ -2,6 +2,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy import constants as spc
 
+# Tableau20 definitions
+tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),  
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),  
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),  
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),  
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)] 
+for i in range(len(tableau20)):  
+    r, g, b = tableau20[i]  
+    tableau20[i] = (r / 255., g / 255., b / 255.)
+
 c = spc.c/1.35 # speed of light in medium
 E_A = 6.63e-34*c/700e-9
 
@@ -37,90 +47,90 @@ fig, ax = plt.subplots(3,3,figsize=(12,12))
 fig.subplots_adjust(hspace = .001, wspace = .001)
 
 #Plot everything
-ax[0,0].plot(time, flux1x4,'r')
+ax[0,0].plot(time, flux1x4,color=tableau20[6])
 ax[0,0].set_ylim((0,12))
 ax[0,0].set_xticklabels(())
 ax[0,0].set_yticklabels(np.arange(0,12+1,2))
 ax2 = ax[0,0].twinx()
-ax2.plot(time, N1x4[:,12],'b')
+ax2.plot(time, N1x4[:,12],color=tableau20[0])
 ax2.set_yticklabels(())
 ax2.set_ylim(0,0.8)
 
-ax[0,1].plot(time, flux2x4,'r')
+ax[0,1].plot(time, flux2x4,color=tableau20[6])
 ax[0,1].set_ylim((0,12))
 ax[0,1].set_xticklabels(())
 ax[0,1].set_yticklabels(np.arange(0,12+1,1))
 ax[0,1].set_yticklabels(())
 ax2 = ax[0,1].twinx()
-ax2.plot(time, N2x4[:,22],'b')
+ax2.plot(time, N2x4[:,22],color=tableau20[0])
 ax2.set_yticklabels(())
 ax2.set_ylim(0,0.8)
 
-ax[0,2].plot(time, flux3x4,'r')
+ax[0,2].plot(time, flux3x4,color=tableau20[6])
 ax[0,2].set_ylim((0,12))
 ax[0,2].set_xticklabels(())
 ax[0,2].set_yticklabels(np.arange(0,12+1,1))
 ax[0,2].set_yticklabels(())
 ax2 = ax[0,2].twinx()
-ax2.plot(time, N3x4[:,32],'b')
+ax2.plot(time, N3x4[:,32],color=tableau20[0])
 ax2.set_yticks(np.arange(0, 0.8+0.1, 0.1))
 ax2.set_ylim(0,0.8)
 
-ax[1,0].plot(time, flux1x2,'r')
+ax[1,0].plot(time, flux1x2,color=tableau20[6])
 ax[1,0].set_ylim((0,7))
 ax[1,0].set_xticklabels(())
 ax[1,0].set_yticklabels(np.arange(0,6+1,1))
 ax2 = ax[1,0].twinx()
-ax2.plot(time, N1x2[:,12],'b')
+ax2.plot(time, N1x2[:,12],color=tableau20[0])
 ax2.set_yticklabels(())
 ax2.set_ylim(0,0.8)
 
-ax[1,1].plot(time, flux2x2,'r')
+ax[1,1].plot(time, flux2x2,color=tableau20[6])
 ax[1,1].set_ylim((0,7))
 ax[1,1].set_xticklabels(())
 ax[1,1].set_yticklabels(np.arange(0,6+1,1))
 ax[1,1].set_yticklabels(())
 ax2 = ax[1,1].twinx()
-ax2.plot(time, N2x2[:,10],'b')
+ax2.plot(time, N2x2[:,10],color=tableau20[0])
 ax2.set_yticklabels(())
 ax2.set_ylim(0,0.8)
 
-ax[1,2].plot(time, flux3x2,'r')
+ax[1,2].plot(time, flux3x2,color=tableau20[6])
 ax[1,2].set_ylim((0,7))
 ax[1,2].set_xticklabels(())
 ax[1,2].set_yticklabels(np.arange(0,6+1,1))
 ax[1,2].set_yticklabels(())
 ax2 = ax[1,2].twinx()
-ax2.plot(time, N3x2[:,12],'b')
+ax2.plot(time, N3x2[:,12],color=tableau20[0])
 ax2.set_yticks(np.arange(0, 0.7+0.1, 0.1))
 ax2.set_ylim(0,0.8)
 
-ax[2,0].plot(time, flux1x1,'r')
+ax[2,0].plot(time, flux1x1,color=tableau20[6])
 ax[2,0].set_ylim((0,4))
 ax[2,0].set_xticklabels([0,10,20,30,40])
 ax[2,0].set_yticklabels(np.arange(0,3+1,0.5))
 ax2 = ax[2,0].twinx()
-ax2.plot(time, N1x1[:,12],'b')
+ax2.plot(time, N1x1[:,12],color=tableau20[0])
 ax2.set_yticklabels(())
 ax2.set_ylim(0,0.6)
 
-ax[2,1].plot(time, flux2x1,'r')
+ax[2,1].plot(time, flux2x1,color=tableau20[6])
 ax[2,1].set_ylim((0,4))
 ax[2,1].set_xticklabels([0,10,20,30,40])
 ax[2,1].set_yticklabels(np.arange(0,3+1,0.5))
 ax[2,1].set_yticklabels(())
 ax2 = ax[2,1].twinx()
-ax2.plot(time, N2x1[:,12],'b')
+ax2.plot(time, N2x1[:,12],color=tableau20[0])
 ax2.set_yticklabels(())
 ax2.set_ylim(0,0.6)
 
-ax[2,2].plot(time, flux3x1,'r')
+ax[2,2].plot(time, flux3x1,color=tableau20[6])
 ax[2,2].set_ylim((0,4))
 ax[2,2].set_xticklabels([0,10,20,30,40,50])
 ax[2,2].set_yticklabels(np.arange(0,3+1,0.5))
 ax[2,2].set_yticklabels(())
 ax2 = ax[2,2].twinx()
-ax2.plot(time, N3x1[:,12],'b')
+ax2.plot(time, N3x1[:,12],color=tableau20[0])
 ax2.set_yticks(np.arange(0, 0.5+0.1, 0.1))
 ax2.set_ylim(0,0.6)
 
@@ -142,4 +152,4 @@ fig.text(0.777, 0.325, r'$L = 3 mm$''\n'r'$I_{G0} = 1\times10^{11}Wm^{-2}$', bbo
 fig.text(0.777, 0.59, r'$L = 3 mm$''\n'r'$I_{G0} = 2\times10^{11}Wm^{-2}$', bbox=dict(facecolor='none',edgecolor='black'))
 fig.text(0.77, 0.855, r'$L = 3 mm$''\n'r'$I_{G0} = 4\times10^{11}Wm^{-2}$', bbox=dict(facecolor='none',edgecolor='black'))
 
-fig.savefig('1Dsystem length and pump variation.png')
+fig.savefig('K1Dsystem length and pump variation.png')
